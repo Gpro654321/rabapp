e41035f2441f8b96bfebe3b893374b2fec47eaa2
gpro <gnanasenthil.pro@gmail.com>
2023-11-09 09:21:15 +0530

    playing with mdscrollview

--- patient_detail_display_window.py
+++ patient_detail_display_window.py
@@ -1,5 +1,7 @@
 from kivymd.uix.screen import MDScreen
+from kivymd.uix.label import MDLabel
 from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.scrollview import MDScrollView
 from kivymd.uix.button import MDRectangleFlatButton
 
 
@@ -13,24 +15,30 @@ class PatientDetailDisplayWindow(MDScreen):
         rec = patient_db.display_from_table_vacdates()
         print(rec)
 
+        try:
+            # clear all widget before adding anything,
+            # because if not cleared every time we move back and forth, 
+            # widgets keep getting added
+            self.ids["box_layout_2"].clear_widgets()
+            print("cleared widgets from box_layout_2")
+        except:
+            pass
+
         for i in rec:
             
-            b = MDLabel(text=str(i[0])+ " - " +str(i[1]))
+            b = MDLabel(text=str(i[0])+ " - " +str(i[1]),size_hint_y=None, height=40)
             # need to modify this code
-            self.add_widget(b)
+            self.ids["box_layout_2"].add_widget(b)
             print("inside PatientDetailLabel")
             print("added widget")
 
     pass
 
         
-'''
 
 class PatientDetailLabel(MDBoxLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+    pass
 
-'''
 
 
 
